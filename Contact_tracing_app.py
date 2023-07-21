f080045bed78f20e204975d1c9d538c6c9d6a8b3
Frances Leigh <fleighbaay@gmail.com>
2023-07-22 00:54:29 +0800

    Pseudocode

--- Contact_tracing_app.py
+++ Contact_tracing_app.py
@@ -1,1 +1,14 @@
 # Main functions
+
+# Create class and initialize empty list to put future contact entries
+
+# Add method to add the entries and its parameters
+    # name, phone number, email, date, vaccination status, covid test, contact person name and number
+
+# Write the entries in a file without overwriting other entries
+
+# Add method to search for matching existing entries
+
+
+
+
