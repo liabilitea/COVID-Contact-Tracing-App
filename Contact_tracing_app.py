b30a388531ed33dc6811f3a864fc698a9d4d5931
Frances Leigh <fleighbaay@gmail.com>
2023-07-22 00:58:39 +0800

    Add function

--- Contact_tracing_app.py
+++ Contact_tracing_app.py
@@ -4,12 +4,9 @@ class ContactTracingApp:
         self.entries = []
 
 # Add method to add the entries and its parameters
+    def add_entry(self, name, phone, email, date, vaccination_status, covid_test, contact_person_name_, contact_person_phone):
     # name, phone number, email, date, vaccination status, covid test, contact person name and number
 
 # Write the entries in a file without overwriting other entries
 
 # Add method to search for matching existing entries
-
-
-
-
