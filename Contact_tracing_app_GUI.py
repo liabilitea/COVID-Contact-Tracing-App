274b512191180ee02979dfe250030be094660bbd
Frances Leigh <fleighbaay@gmail.com>
2023-07-22 10:22:42 +0800

    Pseudocode again

--- Contact_tracing_app_GUI.py
+++ Contact_tracing_app_GUI.py
@@ -1,9 +1,19 @@
 # GUI
 
 # Import
+    # tk
+    # contact tracing app
+
 
 # Create class
+    # title
+    #application
 
 # Set elements per info
+    # Info
 
-# 
+    # Buttons for search add and checklist
+
+# Function to retrieve data from gui
+
+# Function to retrieve key term from gui
