55ad757c16670196ffc2f12286627e98ccf0a8ac
Frances Leigh <fleighbaay@gmail.com>
2023-07-22 10:45:48 +0800

    Class, title, size, and instance

--- Contact_tracing_app_GUI.py
+++ Contact_tracing_app_GUI.py
@@ -6,8 +6,14 @@ from tkinter import messagebox
 from Contact_tracing_app import ContactTracingApp
 
 # Create class
-    # title
-    #application
+
+class ContactTracingAppGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("COVID Contact Tracing App")
+        root.geometry("700x500")
+        self.root.resize(False, False)
+        self.app = ContactTracingApp()
 
 # Set elements per info
     # Info
