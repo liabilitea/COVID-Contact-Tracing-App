b4fa4df2b99984328e1e999ae8bec82b12ca091a
Frances Leigh <fleighbaay@gmail.com>
2023-07-22 13:38:56 +0800

    Test run

--- Main_contact_tracing_app.py
+++ Main_contact_tracing_app.py
@@ -1,4 +1,9 @@
 # Import
+import tkinter as tk
+from Contact_tracing_app_GUI import ContactTracingAppGUI
 
 # Create root window and main module
-
+if __name__ == "__main__":
+    root = tk.Tk()  
+    app_gui = ContactTracingAppGUI(root)
+    root.mainloop()
